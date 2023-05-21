fc5a55b6700f7d7cafe21f6fb61000cd2f0014aa
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-05-20 17:56:23 -0700

    Created the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,46 +1,40 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { useNavbarContext } from "../hooks/useNavbarContext"
 import { useEffect } from "react"
 
-/**
- * Manages the navbar and all it's functionality. It also shows important user information.
- * @param {*} globalUser used to set the current user to a variable to display to the end user.
- * @returns the front-end depending on which button is selected from the navbar.
- */
 const Navbar = () => {
-    /**
-     * This allows us to update the navbar in realtime by making dispatch calls to the navbar context directly.
-     */
-    const { dispatchNavbar, currentPage} = useNavbarContext()
+  const { dispatchNavbar, currentPage } = useNavbarContext();
 
-    useEffect(() => {
+  useEffect(() => {
+  }, []);
 
-
-
-    }, [])
-
-    /**
-     * Returns the current page being displayed to the front-end.
-     */
-    return (
-        <header>
-            <div className="container">
-                {(
-                    <Link to="/">
-                        <h1>Home</h1>
-                    </Link>
-                )}
-                <nav>
-                    {(
-                        <div>
-                            <Link to='/projects'>Projects</Link>
-                            <Link to="/signup">Sign up</Link>
-                        </div>
-                    )}
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="container">
+        {(
+          <Link to="/" className="logo">
+            <h2>Home</h2>
+          </Link>
+        )}
+        {(
+          <Link to="/" className="logo">
+            <h2>Projects</h2>
+          </Link>
+        )}
+        {(
+          <Link to="/" className="logo">
+            <h2>Resume</h2>
+          </Link>
+        )}
+        {(
+          <Link to="/" className="logo">
+            <h2>Contact Me</h2>
+          </Link>
+        )}
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,44 +1,52 @@
 import React from 'react'
-import '../css/projects.css'
 
 const Projects = () => {
-    const projects = [
-      {
-        id: 1,
-        title: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac tincidunt elit.',
-        image: 'project1.jpg',
-        page: 'https://example.com/project1',
-        sourceCode: 'https://example.com/project1'
-      },
-      {
-        id: 2,
-        title: 'Project 2',
-        description: 'Praesent eu vestibulum justo. Donec auctor nibh ac purus feugiat, id tempus risus efficitur.',
-        image: 'project2.jpg',
-        page: 'https://example.com/project2',
-        sourceCode: 'https://example.com/project2'
+  const projects = [
+    {
+      id: 1,
+      title: 'Budgeting React App',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac tincidunt elit.',
+      image: 'project1.jpg',
+      page: 'https://example.com/project1',
+      sourceCode: 'https://example.com/project1'
+    },
+    {
+      id: 2,
+      title: 'Appointment Management System',
+      description: 'Praesent eu vestibulum justo. Donec auctor nibh ac purus feugiat, id tempus risus efficitur.',
+      image: 'project2.jpg',
+      page: 'https://example.com/project2',
+      sourceCode: 'https://example.com/project2'
 
-      },
-    ]
+    },
+    {
+      id: 3,
+      title: 'Routing Optimization Algorithm Program',
+      description: 'Praesent eu vestibulum justo. Donec auctor nibh ac purus feugiat, id tempus risus efficitur.',
+      image: 'project2.jpg',
+      page: 'https://example.com/project2',
+      sourceCode: 'https://example.com/project2'
+
+    }
+  ]
   
-    return (
-      <section id="projects">
-        <h2>Projects</h2>
-        <div className="projects-container">
-          {projects.map((project) => (
-            <div key={project.id} className="project-card">
-              <img src={project.image} alt={project.title} />
-              <div className="project-details">
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <a href={project.page} target="_blank" rel="noopener noreferrer">View Project</a>
-                <a href={project.sourceCode} target="_blank" rel="noopener noreferrer">See Code</a>
-              </div>
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <div className="projects-container">
+        {projects.map((project) => (
+          <div key={project.id} className="project-card">
+            <img src={project.image} alt={project.title} />
+            <div className="project-details">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <a href={project.page} target="_blank" rel="noopener noreferrer">View Project</a>
+              <a href={project.sourceCode} target="_blank" rel="noopener noreferrer">See Code</a>
             </div>
-          ))}
-        </div>
-      </section>
-    )
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
 export default Projects
