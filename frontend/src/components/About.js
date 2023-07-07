66a1dfff3756dfb921afd24c87ef4c50a06de36e
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-07-07 00:47:07 -0700

    Made all titles responsive.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,12 +7,14 @@ const About = () => {
     return (
         <div>
             <h2 className="title">About</h2>
-            <h3 className="skills-description">
-                I was first introduced into programming when I was a kid. I wanted to make my own 
-                video game so I used this program called Game Maker 7. All advanced features required knowledge of c#. I learned c# just well enough 
-                to create my very own first fps game. It wasn't until I became an adult when I realized the true 
-                potential of software development. I'm very passionate about programming and I can't wait to start working together.
-            </h3>
+            <div className="about-parent-section">
+                <h3 className="about-description">
+                    I was first introduced into programming when I was a kid. I wanted to make my own 
+                    video game so I used this program called Game Maker 7. All advanced features required knowledge of c#. I learned c# just well enough 
+                    to create my very own first fps game. It wasn't until I became an adult when I realized the true 
+                    potential of software development. I'm very passionate about programming and I can't wait to start working together.
+                </h3>
+            </div>
         </div>
   )
 }
