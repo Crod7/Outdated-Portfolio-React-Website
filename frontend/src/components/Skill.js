693eb8c9767eb3d7b2ccc8de5d8f8db6d6f876bc
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-07-08 06:12:49 -0700

    Made home and skills views fully responsive. Now resizing will not effect entire app. Navbar remains.

--- frontend/src/components/Skill.js
+++ frontend/src/components/Skill.js
@@ -11,40 +11,42 @@ const Skills = () => {
                 <div className="skill-card" id="technical-container">
                     <img src={'/Portfolio-React-Website/computerIcon.png'} alt={'Technical Skills'} />
                     <div className="spinning-icons">
-                        <h3 className="technical-title-text">{'Technical Skills'}</h3>
-                        <p>{'Java, Python, Node.js, Express.js, JSON, Git, Bash'}</p>
+                        <h3 className="technical-title-text">{'Languages'}</h3>
+                        <p className="skills-subtitle-text">{'JavaScript, CSS, HTML, Java, Python'}</p>
+                        <img src={'/Portfolio-React-Website/js.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/css.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/html.png'} alt={'null/404/notworking.exe'}/>
                         <img src={'/Portfolio-React-Website/java.png'} alt={'null/404/notworking.exe'}/>
                         <img src={'/Portfolio-React-Website/python.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/node.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/express.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/json.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/git.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/gitbash.png'} alt={'null/404/notworking.exe'}/>
+
 
                     </div>
                 </div>
                 <div className="skill-card" id="frontend-container">
                     <img src={'/Portfolio-React-Website/react.png'} alt={'FrontEnd Frameworks'} />
                     <div className="spinning-icons">
-                        <h3 className="frontend-title-text">{'FrontEnd Skills'}</h3>
-                        <p>{'React, Javascript, CSS, HTML'}</p>
+                        <h3 className="frontend-title-text">{'Technologies'}</h3>
+                        <p className="skills-subtitle-text">{'MongoDB, Express.js, React.js, Node.js, MySQL, Git, Bash'}</p>
+                        <img src={'/Portfolio-React-Website/mongo.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/express.png'} alt={'null/404/notworking.exe'}/>
                         <img src={'/Portfolio-React-Website/reactLogo.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/js.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/css.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/html.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/node.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/mysql.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/git.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/gitbash.png'} alt={'null/404/notworking.exe'}/>
+
 
                     </div>
                 </div>
                 <div className="skill-card" id="database-container">
                     <img src={'/Portfolio-React-Website/backendIcon.png'} alt={'Database Skills'} />
                     <div className="spinning-icons">
-                        <h3 className="database-title-text">{'Database Skills'}</h3>
-                        <p>{'MongoDB, MySql, PostgresSql, Postman'}</p>
-                        <img src={'/Portfolio-React-Website/mongo.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/mysql.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/postgres.png'} alt={'null/404/notworking.exe'}/>
-                        <img src={'/Portfolio-React-Website/postman.png'} alt={'null/404/notworking.exe'}/>
-
+                        <h3 className="database-title-text">{'Concepts'}</h3>
+                        <p className="skills-subtitle-text">{'Restful Api, Object-Oriented Programming, Version Control, Data Structures & Algorithms'}</p>
+                        <img src={'/Portfolio-React-Website/whiteIcons/api.svg'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/oops.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/version.png'} alt={'null/404/notworking.exe'}/>
+                        <img src={'/Portfolio-React-Website/dsa.png'} alt={'null/404/notworking.exe'}/>
                     </div>
                 </div>
             </div>
